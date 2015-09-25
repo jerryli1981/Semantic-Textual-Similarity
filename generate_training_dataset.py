2e742c193ad5ebddba54f6b93c0c48539ea4a2a9
Peng Li <jerryli1981@gmail.com>
2015-09-25 17:02:41 -0500

    fix indent problem

--- generate_training_dataset.py
+++ generate_training_dataset.py
@@ -16,27 +16,22 @@ def clean_str(string):
 	return string.strip()
 
 pairSet = set()
-
+		
 with open("train.txt", "wb") as f:
-
-		for folder in folders:
-
-			gss = glob.glob("./"+folder+"/STS.gs."+"*.txt")
-			inputs = glob.glob("./"+folder+"/STS.input."+"*.txt")
-
-			for gs, ip in zip(gss,inputs):
-
-				with open(gs, "rb") as f1, open(ip, "rb") as f2:
-                                        
-					for score, pair in zip(f1,f2):
-						
-                                                items = pair.split('\t')
-                                                first_sent = items[0]
-						first_sent = clean_str(first_sent)
-                                                second_sent = items[1]
-						second_sent = clean_str(second_sent)
+	for folder in folders:
+		gss = glob.glob("./"+folder+"/STS.gs."+"*.txt")
+		inputs = glob.glob("./"+folder+"/STS.input."+"*.txt")
+		
+		for gs, ip in zip(gss,inputs):
+			with open(gs, "rb") as f1, open(ip, "rb") as f2:                            
+				for score, pair in zip(f1,f2):
+					items = pair.split('\t')
+                    first_sent = items[0]
+					first_sent = clean_str(first_sent)
+                    second_sent = items[1]
+					second_sent = clean_str(second_sent)
                                             
-						if pair not in pairSet:
-							f.write(first_sent+"\t"+second_sent+"\t"+score.strip()+"\n")
-							pairSet.add(pair)	 
+					if pair not in pairSet:
+						f.write(first_sent+"\t"+second_sent+"\t"+score.strip()+"\n")
+						pairSet.add(pair)	 
 	
