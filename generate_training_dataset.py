a3ba4a0d3512d38a1ec1cc44bff865a1bf58bff3
Peng Li <jerryli1981@gmail.com>
2015-09-25 15:32:49 -0500

    add clean string function

--- generate_training_dataset.py
+++ generate_training_dataset.py
@@ -1,4 +1,5 @@
 import glob
+import re
 
 _DEBUG = False
 if _DEBUG == True:
@@ -7,17 +8,29 @@ if _DEBUG == True:
 
 folders = ['sts2012', 'sts2013', 'sts2014']
 
+def clean_str(string):
+	string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+	string = re.sub(r"\(.*?\)", "", string)
+	string = re.sub(r"\s{2,}", " ", string)       
+	return string.strip()
+
+pairSet = set()
+
 with open("train.txt", "wb") as f:
 
-	for folder in folders:
+		for folder in folders:
 
-		gss = glob.glob("./"+folder+"/STS.gs."+"*.txt")
-		inputs = glob.glob("./"+folder+"/STS.input."+"*.txt")
+			gss = glob.glob("./"+folder+"/STS.gs."+"*.txt")
+			inputs = glob.glob("./"+folder+"/STS.input."+"*.txt")
 
-		for gs, ip in zip(gss,inputs):
+			for gs, ip in zip(gss,inputs):
 
-			with open(gs, "rb") as f1, open(ip, "rb") as f2:
+				with open(gs, "rb") as f1, open(ip, "rb") as f2:
 
-				for score, pair in zip(f1,f2):
-					f.write(pair.strip()+"\t"+score.strip()+"\n")	 
-
+					for score, pair in zip(f1,f2):
+						
+						pair = clean_str(pair)
+						if pair not in pairSet:
+							f.write(pair.strip()+"\t"+score.strip()+"\n")
+							pairSet.add(pair)	 
+	
