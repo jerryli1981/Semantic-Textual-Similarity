313ad58645fd8e07fa04a8a1794c95a2f19be0e9
Peng Li <jerryli1981@gmail.com>
2015-10-13 03:14:09 -0500

    reorgnize generate training dataset

--- generate_training_dataset.py
+++ generate_training_dataset.py
@@ -1,12 +1,13 @@
 import glob
 import re
+import numpy as np
 
-_DEBUG = False
-if _DEBUG == True:
+from stanford_parser_wrapper import Parser
+
+_DEBUG = True
+if _DEBUG:
     import pdb
     pdb.set_trace()
-
-folders = ['sts2012', 'sts2013', 'sts2014']
 
 def clean_str(string):
     string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
@@ -14,16 +15,21 @@ def clean_str(string):
     string = re.sub(r"\s{2,}", " ", string)       
     return string.strip()
 
-pairSet = set()
-        
-with open("train.txt", "wb") as f:
+def build_training_data():
+
+    parser = Parser() 
+    folders = ['sts2012', 'sts2013', 'sts2014']
+    pairSet = set()   
+
+    dataset = []
     for folder in folders:
         gss = glob.glob("./"+folder+"/STS.gs."+"*.txt")
         inputs = glob.glob("./"+folder+"/STS.input."+"*.txt")
         
         for gs, ip in zip(gss,inputs):
             with open(gs, "rb") as f1, open(ip, "rb") as f2:                            
-                for score, pair in zip(f1,f2):
+                for index, (score, pair) in enumerate(zip(f1,f2)):
+
                     items = pair.split('\t')
                     first_sent = items[0]
                     first_sent = clean_str(first_sent)
@@ -36,6 +42,34 @@ with open("train.txt", "wb") as f:
                         continue
 
                     if pair not in pairSet:
-                        f.write(first_sent+"\t"+second_sent+"\t"+score.strip()+"\n")
-                        pairSet.add(pair)    
-    
+
+                        try:
+                            first_parse_output = parser.parseSentence(first_sent)
+                        except:
+                            print "first_sentence can't be parsing"
+                            #print first_sentence
+                            #traceback.print_exc()
+                            continue
+                        try:
+                            second_parse_output = parser.parseSentence(second_sent)
+                        except:
+                            print "second_sentence can't be parsing"
+                            #print second_sentence
+                            #traceback.print_exc()
+                            continue
+            
+                        datum = {   "score":score.strip(), 
+                                    "text": (first_sentence, second_sentence), 
+                                    "parse":(first_parse_output, second_parse_output),
+                                    "split": np.random.randint(0,cv)
+                                }
+                        revs.append(datum)            
+                        pairSet.add(pair)  
+       
+if __name__ == "main":
+
+    build_training_data()
+
+
+
+
