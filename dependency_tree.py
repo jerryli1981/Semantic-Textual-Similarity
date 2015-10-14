5b16b128a9698fd96303d4c2614a9b03ea732bc2
Peng Li <jerryli1981@gmail.com>
2015-10-13 23:06:08 -0500

    reconstruct dependency tree

--- dependency_tree.py
+++ dependency_tree.py
@@ -1,186 +1,273 @@
-# - an individual node contains the word associated with the node along with 
-#   pointers to its kids and parents. 
-class node:
+UNK = 'UNK'
+
+import networkx as nx
+
+class Node:
 
     def __init__(self, word):
-        if word != None:
-            self.word = word
-            self.kids = []
-            self.parent = []
-            self.finished = 0
-            self.is_word = 1
+        self.word = word
+        self.kids = []
+        self.parent = []
+        self.index = None
 
-            # the "ind" variable stores the look-up index of the word in the 
-            # word embedding matrix We. set this value when the vocabulary is finalized
-            self.ind = -1
+class DTree:
 
-        else:
-            self.is_word = 0
+    def __init__(self, parse_result, label=None, score=None):
 
-class dtree:
+        self.deps = parse_result["deps_ccTree"]
 
-    def __init__(self, word_list):
+        #use lemmas instead of toks 
+        self.lemmas = parse_result["lemmas"]
+            
+        self.label = label
+        self.score = score
+
+        self.root = self.make_tree(self.deps, self.lemmas)
+
+    def make_tree(self, deps, lemmas):        
+        # store tree as adjacent list
         self.nodes = []
-        for word in word_list:
-            self.nodes.append(node(word))
-
-        self.label = -1
-
-
-    def add_edge(self, par, child, rel):
-        self.nodes[par].kids.append( (child, rel ) )
-        self.nodes[child].parent.append( (par, rel) )
-
-
-    # return all non-None nodes
-    def get_nodes(self):
-        return [node for node in self.nodes if node.is_word]
-
-
-    def get_node_inds(self):
-        return [(ind, node) for ind, node in enumerate(self.nodes) if node.is_word]
-
-
-    # get a node from the raw node list
-    def get(self, ind):
-        return self.nodes[ind]
-
-
-    # return the raw text of the sentence
-    def get_words(self):
-        return ' '.join([node.word for node in self.get_nodes()[1:]])
-
-
-    # return raw text of phrase associated with the given node
-    def get_phrase(self, ind):
-
-        node = self.get(ind)
-        words = [(ind, node.word), ]
-        to_do = []
-        for ind, rel in node.kids:
-            to_do.append(self.get(ind))
-            words.append((ind, self.get(ind).word))
-
-        while to_do:
-            curr = to_do.pop()
-
-            # add this kid's kids to to_do
-            if len(curr.kids) > 0:
-                for ind, rel in curr.kids:
-                    words.append((ind, self.get(ind).word))
-                    to_do.insert(0, self.get(ind))  
-
-
-        return ' '.join([word for ind, word in sorted(words, key=itemgetter(0) ) ]).strip()  
-
-
-    def reset_finished(self):
-        for node in self.get_nodes():
-            node.finished = 0
-
-
-    # one tree's error is the sum of the error at all nodes of the tree
-    def error(self):
-        sum = 0.0
-        for node in self.get_nodes():
-            sum += node.ans_error
-
-        return sum
-
-
-    # - this function is not used in the paper, but it enables corrective weighting ala
-    #   Richard's 2014 TACL paper to be implemented
-    # - it counts the size of the subtree rooted at every node in the tree
-    def count_kids(self):
-        for node in self.get_nodes():
-
-            # all nodes include themselves
-            node.count = 1.0
-
-            # no kids, count is just 1
-            if len(node.kids) == 0:
+        for tok in lemmas:
+            self.nodes.append(Node(tok)) 
+        
+        # add dependency edges between nodes
+        rootIdx = None
+        for rel, govIdx, depIdx in deps:
+            if govIdx == -1:
+                rootIdx = depIdx
                 continue
 
-            to_do = [self.get(ind) for ind, rel in node.kids]
+            pNode = Node(lemmas[govIdx])
+            cNode = Node(lemmas[depIdx])
 
-            # otherwise, traverse subtrees
-            while to_do:
-                curr = to_do.pop()
-                node.count += 1
+            self.nodes[govIdx].kids.append((cNode, rel))
+            self.nodes[depIdx].parent.append((pNode, rel))
 
-                # add this kid's kids to to_do
-                if len(curr.kids) > 0:
-                    for ind, rel in curr.kids:
-                        to_do.append(self.get(ind))
+        return self.nodes[rootIdx]
+
+    def mergeWith(self, dtree):
+        
+        #for cycle detection
+        dependencies = []
+        for rel, govIdx, depIdx in self.deps:
+             
+            """
+            if text_govIdx == -1:
+                continue 
+            else:
+                govLemma = self.lemmas[govIdx]
+                govToken = self.tokens[govIdx]
+                         
+            depLemma = self.lemmas[depIdx])
+            depToken = self.tokens[depIdx])
+            """
+
+            dependencies.append((govIdx, depIdx))
+
+        for rel_d, govIdx_d, depIdx_d in dtree.deps:
+            
+            if govIdx_d == -1:
+                continue
+            else:
+                govLemma_d = dtree.lemmas[govIdx_d]
 
 
-# - function that parses the resulting stanford parses
-#   e.g., "nsubj(finalized-5, john-1)"
-def split_relation(text):
-    rel_split = text.split('(')
-    rel = rel_split[0]
-    deps = rel_split[1][:-1]
-    if len(rel_split) != 2:
-        print 'error ', rel_split
-        sys.exit(0)
+            depLemma_d = dtree.lemmas[depIdx_d]
+         
+            add = False
 
-    else:
-        dep_split = deps.split(',')
+            for rel, govIdx, depIdx in self.deps:
 
-        # more than one comma (e.g. 75,000-19)
-        if len(dep_split) > 2:
-
-            fixed = []
-            half = ''
-            for piece in dep_split:
-                piece = piece.strip()
-                if '-' not in piece:
-                    half += piece
-
+                if govIdx == -1:
+                    continue 
                 else:
-                    fixed.append(half + piece)
-                    half = ''
+                    govLemma = self.lemmas[govIdx]
+        
+                             
+                depLemma = self.lemmas[depIdx]
+                
+                govMatch = isMatch(govLemma, govLemma_d)
+                depMatch = isMatch(depLemma, depLemma_d)
+                
+                if govMatch and not depMatch:
+                    add = True
+                    matchedTextGovToken = govLemma
+                    matchedTextGovIdx = govIdx
+                
+                if govMatch and depMatch:
+                    add = False
+                    break
+                         
+            if add:
 
-            print 'fixed: ', fixed
-            dep_split = fixed
+                cNode = Node(depLemma_d)
+                self.nodes.append(cNode)
+                newDepIdx = len(self.nodes)-1
+                dependencies.append((matchedTextGovIdx, newDepIdx))
+                pNode = self.nodes[matchedTextGovIdx]   
+                self.nodes[matchedTextGovIdx].kids.append((cNode, rel_d))
+                cNode.parent.append((pNode, rel_d))
+                                                               
+        G = nx.DiGraph()
+        G.add_edges_from(dependencies)
+        is_dag = nx.is_directed_acyclic_graph(G)
 
-        final_deps = []
-        for dep in dep_split:
-            words = dep.split('-')
-            word = words[0]
-            ind = int(words[len(words) - 1])
+        return self, is_dag
 
-            if len(words) > 2:
-                word = '-'.join([w for w in words[:-1]])
+    
+def isMatch(T, H, synonym = False, entail = False, antonym = False, hypernym = False
+    , hyponym = False):
+    from nltk.corpus import wordnet as wn
+    
+    is_exact_match = False
+    if T == H:
+        is_exact_match = True
 
-            final_deps.append( (ind, word.strip()) )
+    synsets_T = wn.synsets(T)
+    synsets_H = wn.synsets(H)
 
-        return rel, final_deps
+    is_synonym = False
+    is_entail = False
+    is_antonmy = False
+    is_hypernym = False
+    is_hyponym = False
+    
+    if synonym == True:
+        
+        lemmas_T = [str(lemma.name()) for ss in synsets_T for lemma in ss.lemmas()]
+        lemmas_H = [str(lemma.name()) for ss in synsets_H for lemma in ss.lemmas()]
+    
+        c = list(set(lemmas_T).intersection(set(lemmas_H)))
+    
+        if len(c) > 0 or H in lemmas_T:
+            is_synonym = True
+        else:
+            is_synonym = False
 
-# - given a list of all the split relations in a particular sentence,
-#   create a dtree object from that list
-def make_tree(plist):
+    elif entail == True:
+        
+        for s_T in synsets_T:
+            for s_H in synsets_H:
+                if s_H in s_T.entailments():
+                    is_entail = True 
 
-    # identify number of tokens
-    max_ind = -1
-    for rel, deps in plist:
-        for ind, word in deps:
-            if ind > max_ind:
-                max_ind = ind
-
-    # load words into nodes, then make a dependency tree
-    nodes = [None for i in range(0, max_ind + 1)]
-    for rel, deps in plist:
-        for ind, word in deps:
-            nodes[ind] = word
-
-    tree = dtree(nodes)
-
-    # add dependency edges between nodes
-    for rel, deps in plist:
-        par_ind, par_word = deps[0]
-        kid_ind, kid_word = deps[1]
-        tree.add_edge(par_ind, kid_ind, rel)
-
-    return tree  
+    elif antonym == True:
        
+        nega_T = [str(nega.name()) for ss in synsets_T for lemma in ss.lemmas() for nega in lemma.antonyms()]
+        if H in nega_T:
+            is_antonmy = True
+
+    elif hypernym == True:
+            
+        for s_T in synsets_T:
+            for s_H in synsets_H:
+                
+                if s_H in s_T.hyponyms():
+                    is_hypernym = True
+                            
+                if s_T in [synset for path in s_H.hypernym_paths() for synset in path]:
+                    is_hypernym = True           
+
+    elif hyponym == True:
+      
+        for s_T in synsets_T:
+            for s_H in synsets_H:
+                                       
+                if s_T in s_H.hyponyms():
+                    is_hyponym = True
+                
+                if s_H in [synset for path in s_T.hypernym_paths() for synset in path]:
+                    is_hyponym = True           
+    
+    return is_exact_match or is_synonym or is_entail or is_antonmy or is_hypernym or is_hyponym
+
+
+
+
+def loadWordMap():
+    import cPickle as pickle
+    
+    with open('wordMap.bin','r') as fid:
+        return pickle.load(fid)
+
+def buildWordMap():
+    """
+    Builds map of all words in training set
+    to integer values.
+    """
+    from collections import defaultdict
+    import cPickle as pickle
+    file = 'training_dataset'
+    print "Reading dataset to build word map.."
+    trees = []
+    with open(file,'r') as fid:
+        dataset = pickle.load(fid)
+        for index, datum in enumerate(dataset):
+            if index %1000 == 0 :
+                print index
+            first_parse, second_parse = datum["parse"]
+            score = datum["score"]
+            first_depTree = DTree(first_parse, score = score)
+            second_depTree = DTree(second_parse,score = score)
+            mergedTree, isDag = first_depTree.mergeWith(second_depTree)
+            if not isDag:
+                print "merge is not dag"
+                continue
+            trees.append(mergedTree)
+
+    print "Counting words to give each word an index.."
+    
+    words = defaultdict(int)
+    for tree in trees:
+        for node in tree.nodes:
+            words[node.word] += 1
+
+    wordMap = dict(zip(words.iterkeys(),xrange(len(words))))
+    wordMap[UNK] = len(words) # Add unknown as word
+
+    print "Saving wordMap to wordMap.bin"
+    with open('wordMap.bin','w') as fid:
+        pickle.dump(wordMap,fid)
+
+def loadTrees(dataSet='train'):
+    """
+    Loads training trees. Maps leaf node words to word ids.
+    """
+    import cPickle as pickle
+    wordMap = loadWordMap()
+    file = 'training_dataset'
+    print "Loading training_dataset"
+    trees = []
+    with open(file,'r') as fid:
+        dataset = pickle.load(fid)
+        for index, datum in enumerate(dataset):
+            if index %1000 == 0 :
+                print index
+            first_parse, second_parse = datum["parse"]
+            score = datum["score"]
+            first_depTree = DTree(first_parse, score = score)
+            second_depTree = DTree(second_parse,score = score)
+            mergedTree, isDag = first_depTree.mergeWith(second_depTree)
+            if not isDag:
+                print "merge is not dag"
+                continue
+            trees.append(mergedTree)
+
+    for tree in trees:
+        for node in tree.nodes:
+            if node.word not in wordMap:
+                node.index = wordMap[UNK]
+            else:
+                node.index = wordMap[node.word]
+
+    return trees
+
+if __name__=='__main__':
+
+    _Debug_ = False
+    if _Debug_:
+        import pdb
+        pdb.set_trace()
+
+    buildWordMap()
+    #train = loadTrees() 
