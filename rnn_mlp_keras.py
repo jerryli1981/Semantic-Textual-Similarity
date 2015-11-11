b3c19874cef901ccf0189ed085b95d522136a015
Peng Li <jerryli1981@gmail.com>
2015-11-11 03:19:05 -0600

    change hparams

--- rnn_mlp_keras.py
+++ rnn_mlp_keras.py
@@ -1,31 +1,27 @@
+import sys
+import os
+import time
+
+
 import numpy as np
 import theano
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Merge
-from keras.optimizers import SGD, Adam, RMSprop
+from keras.optimizers import SGD, Adam, RMSprop, Adagrad
 from keras.layers.recurrent import LSTM
 
 from scipy.stats import pearsonr
 
 
-def load_data(data, dep_tree, args):
+def load_data(data, dep_tree, maxlen, args):
 
     word2vecs = dep_tree.loadWord2VecMap()
 
     L = word2vecs[:args.wvecDim, :]
 
-    maxlen_1 = 0
-    maxlen_2 = 0
-    Y = np.zeros((len(data), args.outputDim+1), dtype=theano.config.floatX)
+    Y = np.zeros((len(data), args.outputDim+1), dtype=np.float32)
 
     for i, (score, item) in enumerate(data):
-        first_depTree, second_depTree = item
-        first_len = len(first_depTree.nodes)
-        second_len = len(second_depTree.nodes)
-        if first_len > maxlen_1:
-            maxlen_1 = first_len
-        if second_len > maxlen_2:
-            maxlen_2 = second_len
 
         sim = score
         ceil = np.ceil(sim)
@@ -38,8 +34,10 @@ def load_data(data, dep_tree, args):
 
     Y = Y[:, 1:]
 
-    X1 = np.zeros((len(data), maxlen_1, args.wvecDim), dtype=theano.config.floatX)
-    X2 = np.zeros((len(data), maxlen_2, args.wvecDim), dtype=theano.config.floatX)
+    X1 = np.zeros((len(data), maxlen, args.wvecDim), dtype=theano.config.floatX)
+    X2 = np.zeros((len(data), maxlen, args.wvecDim), dtype=theano.config.floatX)
+
+    scores = np.zeros((len(data)), dtype=np.float32)
 
     for i, (score, item) in enumerate(data):
         first_depTree, second_depTree = item
@@ -50,7 +48,21 @@ def load_data(data, dep_tree, args):
         for k, Node in enumerate(second_depTree.nodes):
             X2[i, k] =  L[:, Node.index]
 
-    return X1, X2, Y, maxlen_1, maxlen_2
+        scores[i] = score
+
+    return X1, X2, Y, scores
+
+def iterate_minibatches(inputs1, inputs2, targets, scores, batchsize, shuffle=False):
+    assert len(inputs1) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs1))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs1) - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs1[excerpt], inputs2[excerpt], targets[excerpt], scores[excerpt]
 
 if __name__ == '__main__':
     
@@ -85,24 +97,25 @@ if __name__ == '__main__':
     print "train number %d"%len(trainTrees)
     print "dev number %d"%len(devTrees)
     print "test number %d"%len(testTrees)
-    
-    X1_train, X2_train, Y_train, maxlen_1, maxlen_2 = load_data(trainTrees, tr, args)
-    X1_dev, X2_dev, Y_dev, _,_ = load_data(devTrees, tr, args)
-    X1_test, X2_test, Y_test,_,_ = load_data(testTrees, tr, args)
+
+    maxlen = 36
+    X1_train, X2_train, Y_train, scores_train = load_data(trainTrees, tr, maxlen, args)
+    X1_dev, X2_dev, Y_dev, scores_dev = load_data(devTrees, tr, maxlen, args)
+    X1_test, X2_test, Y_test, scores_test = load_data(testTrees, tr, maxlen, args)
 
     # build the model: 2 stacked LSTM
     print('Build model...')
     first_rep_layer = Sequential()
-    first_rep_layer.add(LSTM(args.wvecDim, return_sequences=True, input_shape=(maxlen_1, args.wvecDim)))
-    first_rep_layer.add(Dropout(0.2))
-    first_rep_layer.add(LSTM(args.wvecDim, return_sequences=False))
+    first_rep_layer.add(LSTM(args.wvecDim, return_sequences=False, input_shape=(maxlen, args.wvecDim)))
+    #first_rep_layer.add(Dropout(0.2))
+    #first_rep_layer.add(LSTM(args.wvecDim, return_sequences=False))
     #first_rep_layer.add(Dropout(0.2))
     #first_rep_layer.add(Dense(args.wvecDim, input_shape=(maxlen_1, args.wvecDim)))
 
     second_rep_layer = Sequential()
-    second_rep_layer.add(LSTM(args.wvecDim, return_sequences=True, input_shape=(maxlen_2, args.wvecDim)))
-    second_rep_layer.add(Dropout(0.2))
-    second_rep_layer.add(LSTM(args.wvecDim, return_sequences=False))
+    second_rep_layer.add(LSTM(args.wvecDim, return_sequences=False, input_shape=(maxlen, args.wvecDim)))
+    #second_rep_layer.add(Dropout(0.2))
+    #second_rep_layer.add(LSTM(args.wvecDim, return_sequences=False))
     #ssecond_rep_layer.add(Dropout(0.2))
     #second_rep_layer.add(Dense(args.wvecDim, input_shape=(maxlen_2, args.wvecDim)))
 
@@ -116,52 +129,63 @@ if __name__ == '__main__':
 
     model = Sequential()
     model.add(Merge([mul_layer, sub_layer], mode='sum'))
-    model.add(Dense(args.hiddenDim, input_shape=(args.wvecDim,), init='uniform'))
-    model.add(Activation('tanh'))
+    #model.add(Dense(args.hiddenDim, input_shape=(args.wvecDim,), init='uniform'))
+    model.add(Activation('sigmoid'))
     #model.add(Dropout(0.2))
     #model.add(Dense(50))
     #model.add(Activation('relu'))
-    model.add(Dropout(0.2))
+    #model.add(Dropout(0.2))
     model.add(Dense(5, init='uniform'))
     model.add(Activation('softmax'))
 
-    rms = RMSprop()
-    sgd = SGD(lr=0.1, decay=1e-6, mementum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    #rms = RMSprop()
+    #sgd = SGD(lr=0.1, decay=1e-6, mementum=0.9, nesterov=True)
+    adagrad = Adagrad(lr=0.01)
+    model.compile(loss='categorical_crossentropy', optimizer=adagrad)
 
-    print "begin to train model"
+
     #model.fit([X1_train, X2_train], Y_train, batch_size=args.minibatch, nb_epoch=args.epochs, show_accuracy=True, verbose=2, validation_data=([X1_dev, X2_dev],  Y_dev))
-    model.fit([X1_train, X2_train], Y_train, batch_size=args.minibatch, nb_epoch=args.epochs)
+    #model.fit([X1_train, X2_train], Y_train, batch_size=args.minibatch, nb_epoch=args.epochs)
 
-    corrects = [] 
-    for i, (score, item) in enumerate(devTrees):
-        corrects.append(score)
+    print("Starting training...")
+    best_dev_score = .0
+    for epoch in range(args.epochs):
+        # In each epoch, we do a full pass over the training data:
+        train_err = 0
+        train_batches = 0
+        start_time = time.time()
+        for batch in iterate_minibatches(X1_train, X2_train, Y_train, scores_train, args.minibatch, shuffle=True):
+            inputs1, inputs2, targets, _ = batch
+            err = model.train_on_batch([inputs1, inputs2], targets)
+            train_batches += 1
+            train_err += err
 
-    preds = model.predict([X1_dev, X2_dev])
-    predictScores = preds.dot(np.array([1,2,3,4,5]))
-    guesses = predictScores.tolist()
-    print pearsonr(corrects,guesses)[0]
-    
-    epoch = 0
-    best_dev_score  = 0.
-    while (epoch < args.epochs):
-        epoch = epoch + 1 
+        # And a full pass over the validation data:
+        
+        val_err = 0
+        val_batches = 0
+        val_pearson = 0
+        for batch in iterate_minibatches(X1_dev, X2_dev, Y_dev, scores_dev, 500, shuffle=False):
+            inputs1, inputs2, targets, scores = batch
+            preds = model.predict_proba([inputs1, inputs2])
+            #val_err += 
+            val_batches += 1
 
-        batches = [trainTrees[idx : idx + args.minibatch] for idx in xrange(0, len(trainTrees), args.minibatch)]
+            predictScores = preds.dot(np.array([1,2,3,4,5]))
+            guesses = predictScores.tolist()
+            scores = scores.tolist()
+            pearson_score = pearsonr(scores,guesses)[0]
+            val_pearson += pearson_score 
 
-        for index, batchData in enumerate(batches):
-            X1_train_batch, X2_train_batch, Y_train_batch, _,_= load_data(batchData, tr, args)
-            model.train_on_batch([X1_train_batch, X2_train_batch], Y_train_batch)
 
-        preds = model.predict_proba([X1_dev, X2_dev])
-        predictScores = preds.dot(np.array([1,2,3,4,5]))
-        guesses = predictScores.tolist()
-        dev_score = pearsonr(corrects,guesses)[0]
-        if dev_score > best_dev_score:
-            best_dev_score = dev_score
-            print "iter:%d dev_score: %f best_dev_score %f"%(epoch, dev_score, best_dev_score)
-        else:
-            print "iter:%d dev_score: %f"%(epoch, dev_score)
+        # Then we print the results for this epoch:
+        print("Epoch {} of {} took {:.3f}s".format(
+            epoch + 1, args.epochs, time.time() - start_time))
+        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+        #print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
 
-    
+
+        print("  validation pearson:\t\t{:.2f} %".format(
+            val_pearson / val_batches * 100))
+
     
