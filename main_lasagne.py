6d37b0f635363a82d31b1dad19a92d91e68cbfcd
Peng Li <jerryli1981@gmail.com>
2015-11-23 22:44:43 -0600

    fix bugs in pool shape

--- main_lasagne.py
+++ main_lasagne.py
@@ -510,8 +510,8 @@ def build_network_2dconv(args, input1_var, input1_mask_var,
     #pool_size=(maxlen-3+1,1)
 
     #two conv pool layer
-    filter_size=(3, 5)
-    pool_size=(2,2)
+    filter_size=(10, 100)
+    pool_size=(4,4)
 
     input_1 = InputLayer((None, maxlen),input_var=input1_var)
     batchsize, seqlen = input_1.input_var.shape
@@ -523,13 +523,13 @@ def build_network_2dconv(args, input1_var, input1_mask_var,
 
     conv2d_1 = Conv2DLayer(reshape_1, num_filters=num_filters, filter_size=(filter_size), stride=stride, 
         nonlinearity=rectify,W=GlorotUniform()) #(None, 100, 34, 1)
-
-    maxpool_1 = MaxPool2DLayer(conv2d_1, pool_size=pool_size) #(None, 100, 1, 1)
-
-    conv2d_1 = Conv2DLayer(maxpool_1, num_filters=num_filters, filter_size=filter_size, stride=stride, 
+    maxpool_1 = MaxPool2DLayer(conv2d_1, pool_size=pool_size) #(None, 100, 1, 1) 
+  
+    filter_size_2=(4, 10)
+    pool_size_2=(2,2)
+    conv2d_1 = Conv2DLayer(maxpool_1, num_filters=num_filters, filter_size=filter_size_2, stride=stride, 
         nonlinearity=rectify,W=GlorotUniform()) #(None, 100, 34, 1)
-    maxpool_1 = MaxPool2DLayer(conv2d_1, pool_size=pool_size) #(None, 100, 1, 1)
-
+    maxpool_1 = MaxPool2DLayer(conv2d_1, pool_size=pool_size_2) #(None, 100, 1, 1) (None, 100, 1, 20)
 
     forward_1 = FlattenLayer(maxpool_1) #(None, 100) #(None, 50400)
  
@@ -544,9 +544,9 @@ def build_network_2dconv(args, input1_var, input1_mask_var,
         nonlinearity=rectify,W=GlorotUniform()) #(None, 100, 34, 1)
     maxpool_2 = MaxPool2DLayer(conv2d_2, pool_size=pool_size) #(None, 100, 1, 1)
 
-    conv2d_2 = Conv2DLayer(maxpool_2, num_filters=num_filters, filter_size=filter_size, stride=stride, 
+    conv2d_2 = Conv2DLayer(maxpool_2, num_filters=num_filters, filter_size=filter_size_2, stride=stride, 
         nonlinearity=rectify,W=GlorotUniform()) #(None, 100, 34, 1)
-    maxpool_2 = MaxPool2DLayer(conv2d_2, pool_size=pool_size) #(None, 100, 1, 1)
+    maxpool_2 = MaxPool2DLayer(conv2d_2, pool_size=pool_size_2) #(None, 100, 1, 1)
 
     forward_2 = FlattenLayer(maxpool_2) #(None, 100)
 
